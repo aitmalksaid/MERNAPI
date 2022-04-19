c5f48e68ee987bb45d0bfc57889252dcb47744e6
said aitmalk <aitmalksaid@gmail.com>
2022-04-19 15:25:48 +0100

    1er version

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './App.css';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h2>version 1</h2>
+      <p>lorem10</p>
+    </div>
+  );
+}
+
+export default App;
